3c9ab5ef61cff8de21dbe076be22633e4e0d8426
Raymond de Wit <rdewit@schubergphilis.com>
2017-01-20 12:17:10 +0100

    Fixed bug where custom types weren't handled correctly

--- src/utils.js
+++ src/utils.js
@@ -6,37 +6,23 @@ function capitalize(str) {
 }
 
 function determineType(field, message, messageEnums, globalEnums) {
-  // Check if it's a enum on message level
   if (messageEnums.indexOf(`${message.name}${capitalize(field.name)}`) > -1) {
     return 'message-enum';
   }
 
-  // Check if it's a global enum
   if (globalEnums.indexOf(field.type) > -1) {
     return 'global-enum';
   }
 
-  // Check if it's and standard type
-  var tsType = types[field.type] || 'any';
-
-  // Check the rules to see if it's an map or list
-  switch(field.rule) {
-    case 'repeated':
-      tsType = 'List';
-      break;
-
-    case 'map':
-      tsType = 'Map';
-      break;
+  if (field.repeated) {
+    return 'List';
   }
 
-  // Check for other entities
-  // @TODO remove optional?
-  if (field.rule === 'optional' && tsType === 'any') {
-    tsType = 'custom';
+  if (field.map) {
+    return 'Map';
   }
 
-  return tsType;
+  return types[field.type] || 'custom';
 }
 
 function isCustomType(field) {
